7d3481f9458241523116266d9c4c6592082aa346
Pierre Schmitz <pierre@archlinux.de>
2011-07-24 21:32:48 +0200

    Fix test for use with recent namcap releases

--- test/test.d/packages.sh
+++ test/test.d/packages.sh
@@ -4,8 +4,8 @@ curdir=$(readlink -e $(dirname $0))
 . "${curdir}/../lib/common.inc"
 
 testPackages() {
-	# TODO: namcap -r depends fails with i686 packages
-	find "${pkgdir}" -name "*${PKGEXT}" -exec namcap -e depends {} + || fail 'namcap failed'
+	# TODO: namcap -r sodepends fails with i686 packages
+	find "${pkgdir}" -name "*${PKGEXT}" -exec namcap -e sodepends,pkgnameindesc {} + || fail 'namcap failed'
 }
 
 . "${curdir}/../lib/shunit2"

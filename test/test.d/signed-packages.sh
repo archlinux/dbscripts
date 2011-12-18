2b7bb0c6b7aa4f7a43c82db1cf9a18d27600c62e
Pierre Schmitz <pierre@archlinux.de>
2011-12-18 14:16:30 +0100

    Validate package signatures on db-update

--- test/test.d/signed-packages.sh
+++ test/test.d/signed-packages.sh
@@ -5,9 +5,27 @@ curdir=$(readlink -e $(dirname $0))
 
 testAddUnsignedPackage() {
 	releasePackage extra 'pkg-simple-a' 'i686'
-	# remove any signature
 	rm "${STAGING}"/extra/*.sig
 	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is missing!"
+}
+
+testAddInvalidSignedPackage() {
+	local p
+	releasePackage extra 'pkg-simple-a' 'i686'
+	for p in "${STAGING}"/extra/*${PKGEXT}; do
+		unxz $p
+		xz -0 ${p%%.xz}
+	done
+	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is invalid!"
+}
+
+testAddBrokenSignature() {
+	local s
+	releasePackage extra 'pkg-simple-a' 'i686'
+	for s in "${STAGING}"/extra/*.sig; do
+		echo 0 > $s
+	done
+	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is broken!"
 }
 
 . "${curdir}/../lib/shunit2"

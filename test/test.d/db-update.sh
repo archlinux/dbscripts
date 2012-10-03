b0047186ad33c5ef8bb6560dd9ba4d8a724dabce
Pierre Schmitz <pierre@archlinux.de>
2012-10-03 14:49:02 +0200

    test: Do not suppress devtools output

--- test/test.d/db-update.sh
+++ test/test.d/db-update.sh
@@ -82,7 +82,7 @@ testUpdateAnyPackage() {
 	pushd "${TMP}/svn-packages-copy/pkg-any-a/trunk/" >/dev/null
 	sed 's/pkgrel=1/pkgrel=2/g' -i PKGBUILD
 	svn commit -q -m"update pkg to pkgrel=2" >/dev/null
-	sudo extra-i686-build >/dev/null 2>&1
+	sudo extra-i686-build
 	mv pkg-any-a-1-2-any.pkg.tar.xz "${pkgdir}/pkg-any-a/"
 	popd >/dev/null
 
@@ -100,7 +100,7 @@ testUpdateAnyPackageToDifferentRepositoriesAtOnce() {
 	pushd "${TMP}/svn-packages-copy/pkg-any-a/trunk/" >/dev/null
 	sed 's/pkgrel=1/pkgrel=2/g' -i PKGBUILD
 	svn commit -q -m"update pkg to pkgrel=2" >/dev/null
-	sudo extra-i686-build >/dev/null 2>&1
+	sudo extra-i686-build
 	mv pkg-any-a-1-2-any.pkg.tar.xz "${pkgdir}/pkg-any-a/"
 	popd >/dev/null
 

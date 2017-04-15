62fab76a3ec1ee9caf3abab5eb2c73b8f06fe4ce
Pierre Schmitz <pierre@archlinux.de>
2017-04-15 20:19:15 +0200

    Fix package test
    namcap does not return a non-zero code on error

--- test/test.d/packages.sh
+++ test/test.d/packages.sh
@@ -1,4 +1,7 @@
 testPackages() {
-	# TODO: namcap -r sodepends fails with i686 packages
-	find "${pkgdir}" -name "*${PKGEXT}" -exec namcap -e sodepends,pkgnameindesc {} + || fail 'namcap failed'
+	local result
+	for pkg in $(find "${pkgdir}" -name "*${PKGEXT}"); do
+		result=$(namcap -e pkgnameindesc ${pkg})
+		[[ -n "${result}" ]] && fail "${result}"
+	done;
 }

10439da698c8eb723b1637b803cb37370245a2f8
Pierre Schmitz <pierre@archlinux.de>
2017-04-18 16:33:21 +0200

    db-repo-add: Fix adding of 'any' packages

--- test/test.d/db-repo-add.sh
+++ test/test.d/db-repo-add.sh
@@ -1,4 +1,4 @@
-testAddSimplePackages() {
+testRepoAddSimplePackages() {
 	local arches=('i686' 'x86_64')
 	local pkgs=('pkg-simple-a' 'pkg-simple-b')
 	local pkgbase
@@ -21,7 +21,7 @@ testAddSimplePackages() {
 	done
 }
 
-testAddMultiplePackages() {
+testRepoAddMultiplePackages() {
 	local arches=('i686' 'x86_64')
 	local pkgs=('pkg-simple-a' 'pkg-simple-b')
 	local pkgbase
@@ -42,6 +42,29 @@ testAddMultiplePackages() {
 	for pkgbase in ${pkgs[@]}; do
 		for arch in ${arches[@]}; do
 			checkPackageDB extra ${pkgbase}-1-1-${arch}.pkg.tar.xz ${arch}
+		done
+	done
+}
+
+testRepoAddAnyPackages() {
+	local arches=('i686' 'x86_64')
+	local pkgs=('pkg-any-a' 'pkg-any-b')
+	local pkgbase
+	local arch
+
+	for pkgbase in ${pkgs[@]}; do
+		cp "${pkgdir}/${pkgbase}/${pkgbase}-1-1-any.pkg.tar.xz" "${FTP_BASE}/${PKGPOOL}/"
+		touch "${FTP_BASE}/${PKGPOOL}/${pkgbase}-1-1-any.pkg.tar.xz.sig"
+		for arch in ${arches[@]}; do
+			ln -s "${FTP_BASE}/${PKGPOOL}/${pkgbase}-1-1-any.pkg.tar.xz" "${FTP_BASE}/extra/os/${arch}/"
+			ln -s "${FTP_BASE}/${PKGPOOL}/${pkgbase}-1-1-any.pkg.tar.xz.sig" "${FTP_BASE}/extra/os/${arch}/"
+		done
+		../db-repo-add extra any ${pkgbase}-1-1-any.pkg.tar.xz
+	done
+
+	for pkgbase in ${pkgs[@]}; do
+		for arch in ${arches[@]}; do
+			checkPackageDB extra ${pkgbase}-1-1-any.pkg.tar.xz ${arch}
 		done
 	done
 }

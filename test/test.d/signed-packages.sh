41f4b54c56702610e23ba9130b2022c3f0fe5961
Pierre Schmitz <pierre@archlinux.de>
2017-04-18 17:09:21 +0200

    db-update: Check that packages with invalid signatures wont get added

--- test/test.d/signed-packages.sh
+++ test/test.d/signed-packages.sh
@@ -3,13 +3,15 @@ testAddSignedPackage() {
 	../db-update || fail "db-update failed!"
 }
 
-testAddUnsignedPackage() {
+testAddUnsignedPackageFails() {
 	releasePackage extra 'pkg-simple-a' 'i686'
 	rm "${STAGING}"/extra/*.sig
 	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is missing!"
+
+	checkRemovedPackage extra pkg-simple-a-1-1-i686.pkg.tar.xz i686
 }
 
-testAddInvalidSignedPackage() {
+testAddInvalidSignedPackageFails() {
 	local p
 	releasePackage extra 'pkg-simple-a' 'i686'
 	for p in "${STAGING}"/extra/*${PKGEXT}; do
@@ -17,13 +19,17 @@ testAddInvalidSignedPackage() {
 		xz -0 ${p%%.xz}
 	done
 	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is invalid!"
+
+	checkRemovedPackage extra pkg-simple-a-1-1-i686.pkg.tar.xz i686
 }
 
-testAddBrokenSignature() {
+testAddBrokenSignatureFails() {
 	local s
 	releasePackage extra 'pkg-simple-a' 'i686'
 	for s in "${STAGING}"/extra/*.sig; do
 		echo 0 > $s
 	done
 	../db-update >/dev/null 2>&1 && fail "db-update should fail when a signature is broken!"
+
+	checkRemovedPackage extra pkg-simple-a-1-1-i686.pkg.tar.xz i686
 }
